784f70b64b42de71c147712c1fad9827bb1fca1f
burnout909 <newhorizon2232@gmail.com>
2025-04-05 03:36:43 +0900

    fix loading component

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -7,17 +7,17 @@ export default function Spinner() {
       viewBox="0 0 24 24"
     >
       <circle
-        className="opacity-70"
+        className="opacity-40"
         cx="12"
         cy="12"
         r="10"
         stroke="currentColor"
-        strokeWidth="4"
+        strokeWidth="3"
       />
       <path
         className="opacity-100"
         fill="currentColor"
-        d="M4 12a8 8 0 018-8v8z"
+        d="M12 2a10 10 0 0 1 10 10h-2.5a7.5 7.5 0 0 0-7.5-7.5V2z"
       />
     </svg>
   );
